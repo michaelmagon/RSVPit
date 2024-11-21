ae07da56713b29efbced316751512e22d16ee213
michaelmagon <michael_magon@outlook.com>
2024-11-21 21:52:10 +0800

    debug redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :debug_redirect
+
+  def debug_redirect
+    Rails.logger.info("Redirecting: #{request.host} to #{request.url}")
+  end
 end
